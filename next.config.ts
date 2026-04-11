d3b37cd64fade46b8b6e0890fb932a9b5fdf47d7
Ov3rkill15 <lwnsryd24@gmail.com>
2026-04-12 00:21:27 +0700

    itu doang

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/dashboard-proximiti',
+  basePath: '/peta-proximiti',
   images: {
     unoptimized: true
   }
